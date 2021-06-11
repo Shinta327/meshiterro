a11f76d3f9d8c97a7cf80cbde4ee7031351da077
ShintaKanayama <shinta327tuba@gmail.com>
2021-06-11 01:30:21 +0000

    [Finish]4-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6512d6bc2a56d6be83cc5555ab8bfe5fd221e1faef1e80d207be9da070242018ab8d68f5bc8c7044a0d72da0c546af791bbd9a58cf5349fe3894b00d169b79fc'
